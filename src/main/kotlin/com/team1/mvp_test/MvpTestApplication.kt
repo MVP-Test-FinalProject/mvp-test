b14bd74190f5fd656166cffd493cf817f73e2b31
RID <minzoo20@highway0526.onmicrosoft.com>
2024-07-16 10:24:32 +0900

    :tada: create: Initialize new project for MVP test

---
+++ src/main/kotlin/com/team1/mvp_test/MvpTestApplication.kt
@@ -0,0 +1,11 @@
+package com.team1.mvp_test
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class MvpTestApplication
+
+fun main(args: Array<String>) {
+	runApplication<MvpTestApplication>(*args)
+}
