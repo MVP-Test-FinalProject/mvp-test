121576d5bd6cae419c8307d9ea25e290d6c09a8d
AWKRID <minzoo202738@gmail.com>
2024-07-16 12:40:45 +0900

    :sparkles: feat: Implement GlobalExceptionHandler

---
+++ src/main/kotlin/com/team1/mvp_test/domain/common/exception/GlobalExceptionHandler.kt
@@ -0,0 +1,34 @@
+package com.team1.mvp_test.domain.common.exception
+
+import com.team1.mvp_test.domain.common.exception.dto.ErrorResponse
+import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.ExceptionHandler
+import org.springframework.web.bind.annotation.RestControllerAdvice
+
+
+@RestControllerAdvice
+class GlobalExceptionHandler {
+
+    @ExceptionHandler(ModelNotFoundException::class)
+    fun handleModelNotFound(e: ModelNotFoundException): ResponseEntity<ErrorResponse> {
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(ErrorResponse(e.message))
+    }
+
+    @ExceptionHandler(IllegalStateException::class)
+    fun handlerIllegalStateException(e: IllegalStateException): ResponseEntity<ErrorResponse> {
+        return ResponseEntity.status(HttpStatus.CONFLICT).body(ErrorResponse(e.message))
+    }
+
+    @ExceptionHandler(IllegalArgumentException::class)
+    fun handlerIllegalArgumentException(e: IllegalArgumentException): ResponseEntity<ErrorResponse> {
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(ErrorResponse(e.message))
+    }
+
+    @ExceptionHandler(PasswordIncorrectException::class)
+    fun handlerPasswordIncorrectException(e: PasswordIncorrectException): ResponseEntity<ErrorResponse> {
+        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(ErrorResponse(e.message))
+    }
+
+
+}
---
+++ src/main/kotlin/com/team1/mvp_test/domain/common/exception/dto/ErrorResponse.kt
@@ -0,0 +1,5 @@
+package com.team1.mvp_test.domain.common.exception.dto
+
+data class ErrorResponse(
+    val message: String?
+)
