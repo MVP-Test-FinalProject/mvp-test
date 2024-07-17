9f70111f248774c080ec2792fa6bfcba70d5f3df
pcouqc <nagapen@nate.com>
2024-07-17 15:34:09 +0900

    :tada: create : create

---
+++ src/main/kotlin/com/team1/mvp_test/domain/member/repository/MemberTestRepository.kt
@@ -0,0 +1,9 @@
+package com.team1.mvp_test.domain.member.repository
+
+import com.team1.mvp_test.domain.member.model.MemberTest
+import org.springframework.data.jpa.repository.JpaRepository
+
+interface MemberTestRepository: JpaRepository<MemberTest, Long>  {
+
+    fun findByMemberIdAndTestId(memberId: Long, testId: Long): MemberTest
+}
