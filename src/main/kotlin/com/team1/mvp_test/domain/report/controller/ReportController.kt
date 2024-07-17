fd13cf19613879fb8e34f54e8756902f818086b5
pcouqc <nagapen@nate.com>
2024-07-17 14:41:29 +0900

    :tada: create : create

---
+++ src/main/kotlin/com/team1/mvp_test/domain/report/controller/ReportController.kt
@@ -0,0 +1,72 @@
+package com.team1.mvp_test.domain.report.controller
+
+import com.team1.mvp_test.domain.report.service.ReportService
+import com.team1.mvp_test.domain.report.dto.ApproveReportRequest
+import com.team1.mvp_test.domain.report.dto.ApproveReportResponse
+import com.team1.mvp_test.domain.report.dto.ReportResponse
+import com.team1.mvp_test.domain.report.dto.UpdateReportRequest
+import com.team1.mvp_test.infra.security.UserPrincipal
+import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
+import org.springframework.security.core.annotation.AuthenticationPrincipal
+import org.springframework.web.bind.annotation.*
+
+@RestController
+@RequestMapping("mvp-test/{test-id}/steps/{step-id}/reports")
+class ReportController(
+    private val reportService: ReportService
+) {
+
+    @PostMapping
+    fun createReport(
+        @PathVariable("test-id") testId: Long,
+        @PathVariable("step-id") stepId: Long,
+        @RequestBody request: UpdateReportRequest,
+        @AuthenticationPrincipal userPrincipal: UserPrincipal
+    ): ResponseEntity<ReportResponse> {
+        return ResponseEntity
+            .status(HttpStatus.CREATED)
+            .body(reportService.createReport(stepId, request, userPrincipal.id))
+    }
+
+    @PutMapping("/{report-id}")
+    fun updateReport(
+        @PathVariable("test-id") testId: Long,
+        @PathVariable("step-id") stepId: Long,
+        @PathVariable("report-id") reportId: Long,
+        @AuthenticationPrincipal userPrincipal: UserPrincipal,
+        @RequestBody request: UpdateReportRequest
+    ): ResponseEntity<ReportResponse> {
+        return ResponseEntity
+            .status(HttpStatus.OK)
+            .body(reportService.updateReport(testId, stepId, reportId, request, userPrincipal.id))
+    }
+
+    @DeleteMapping("/{report-id}")
+    fun deleteReport(
+        @PathVariable("test-id") testId: Long,
+        @PathVariable("step-id") stepId: Long,
+        @PathVariable("report-id") reportId: Long,
+        @AuthenticationPrincipal userPrincipal: UserPrincipal,
+    ): ResponseEntity<Unit> {
+        reportService.deleteReport(testId, stepId, reportId, userPrincipal.id)
+        return ResponseEntity
+            .status(HttpStatus.NO_CONTENT)
+            .build()
+    }
+
+
+    @PutMapping("/{report-id}/approve")
+    fun approveReport(
+        @PathVariable("test-id") testId: Long,
+        @PathVariable("step-id") stepId: Long,
+        @PathVariable("report-id") reportId: Long,
+        @AuthenticationPrincipal userPrincipal: UserPrincipal,
+        @RequestBody request: ApproveReportRequest
+    ): ResponseEntity<ApproveReportResponse> {
+        return ResponseEntity
+            .status(HttpStatus.OK)
+            .body(reportService.approveReport(testId, stepId, reportId, request, userPrincipal.id))
+    }
+
+}
