5acc6d930a607672f8ec190ced6735cada399b53
pcouqc <nagapen@nate.com>
2024-07-19 15:13:34 +0900

    :fire: remove : Remove unused import

--- src/main/kotlin/com/team1/mvp_test/domain/report/model/Report.kt
+++ src/main/kotlin/com/team1/mvp_test/domain/report/model/Report.kt
@@ -1,6 +1,5 @@
 package com.team1.mvp_test.domain.report.model
 
-import com.team1.mvp_test.common.error.ReportErrorMessage
 import com.team1.mvp_test.domain.member.model.MemberTest
 import com.team1.mvp_test.domain.step.model.Step
 import jakarta.persistence.*
