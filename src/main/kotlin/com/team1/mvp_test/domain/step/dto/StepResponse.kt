6c8e695df6fea89da6f74045413125003b3b5bac
gooddle <vptl6559@gmail.com>
2024-07-19 18:59:38 +0900

    :recycle: refactor : Update StepResponse to handle nullable guidelineUrl

--- src/main/kotlin/com/team1/mvp_test/domain/step/dto/StepResponse.kt
+++ src/main/kotlin/com/team1/mvp_test/domain/step/dto/StepResponse.kt
@@ -3,24 +3,24 @@ package com.team1.mvp_test.domain.step.dto
 import com.team1.mvp_test.domain.step.model.Step
 
 data class StepResponse(
-    val stepId : Long,
-    val testId : Long,
-    val title : String,
-    val requirement : String,
-    val guidelineUrl :String,
-    val stepOrder : Int,
-    val reward : Int,
-){
-    companion object{
-        fun from(step : Step) : StepResponse {
+    val stepId: Long,
+    val testId: Long,
+    val title: String,
+    val requirement: String,
+    val guidelineUrl: String?,
+    val stepOrder: Int,
+    val reward: Int,
+) {
+    companion object {
+        fun from(step: Step): StepResponse {
             return StepResponse(
-               stepId = step.id!!,
-               testId = step.mvpTest.id!!,
-               title = step.title,
-               requirement = step.requirement,
-               guidelineUrl = step.guidelineUrl!!,
+                stepId = step.id!!,
+                testId = step.mvpTest.id!!,
+                title = step.title,
+                requirement = step.requirement,
+                guidelineUrl = step.guidelineUrl!!,
                 stepOrder = step.stepOrder,
-               reward = step.reward,
+                reward = step.reward,
             )
         }
     }
