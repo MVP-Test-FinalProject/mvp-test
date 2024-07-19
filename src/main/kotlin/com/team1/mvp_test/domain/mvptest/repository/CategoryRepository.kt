f358069f2a965f3fc652a34710d18d7242f74d47
pcouqc <nagapen@nate.com>
2024-07-19 15:09:44 +0900

    :sparkles: feat : Add annotation was missing

--- src/main/kotlin/com/team1/mvp_test/domain/mvptest/repository/CategoryRepository.kt
+++ src/main/kotlin/com/team1/mvp_test/domain/mvptest/repository/CategoryRepository.kt
@@ -2,7 +2,9 @@ package com.team1.mvp_test.domain.mvptest.repository
 
 import com.team1.mvp_test.domain.mvptest.model.Category
 import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.stereotype.Repository
 
+@Repository
 interface CategoryRepository: JpaRepository<Category, Long> {
     fun findByName(name: String): Category?
 }
--- src/main/kotlin/com/team1/mvp_test/domain/mvptest/repository/MvpTestRepository.kt
+++ src/main/kotlin/com/team1/mvp_test/domain/mvptest/repository/MvpTestRepository.kt
@@ -2,6 +2,8 @@ package com.team1.mvp_test.domain.mvptest.repository
 
 import com.team1.mvp_test.domain.mvptest.model.MvpTest
 import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.stereotype.Repository
 
+@Repository
 interface MvpTestRepository : JpaRepository<MvpTest, Long>{
 }
