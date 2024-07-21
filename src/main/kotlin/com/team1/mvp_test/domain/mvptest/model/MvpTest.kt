38c1757281b2bf050f86a30058fb984fb0c7ae56
LSH <fhzktm@naver.com>
2024-07-21 22:27:48 +0900

    :sparkles: feat: add Mvptest validation

--- src/main/kotlin/com/team1/mvp_test/domain/mvptest/model/MvpTest.kt
+++ src/main/kotlin/com/team1/mvp_test/domain/mvptest/model/MvpTest.kt
@@ -3,6 +3,11 @@ package com.team1.mvp_test.domain.mvptest.model
 import com.team1.mvp_test.domain.mvptest.constant.RecruitType
 import com.team1.mvp_test.domain.mvptest.dto.mvptest.UpdateMvpTestRequest
 import jakarta.persistence.*
+import jakarta.validation.constraints.Max
+import jakarta.validation.constraints.Min
+import jakarta.validation.constraints.NotBlank
+import jakarta.validation.constraints.Pattern
+import jakarta.validation.constraints.Size
 import java.time.LocalDateTime
 
 @Table(name = "mvp_test")
@@ -15,6 +20,8 @@ class MvpTest(
     @Column(name = "enterprise_id")
     val enterpriseId : Long,
 
+    @field:NotBlank
+    @field:Size(min=1, max = 50)
     @Column(name = "mvp_name")
     var mvpName: String,
 
@@ -39,9 +46,12 @@ class MvpTest(
     @Column(name = "mvp_url")
     var mvpUrl : String,
 
+    @field:Min(10000)
     @Column(name = "reword_budget")
     var rewardBudget : Int,
 
+    @field:Min(1)
+    @field:Max(80)
     @Column(name = "requirement_min_age")
     var requirementMinAge : Int?,
 
@@ -51,15 +61,16 @@ class MvpTest(
     @Column(name = "requirement_sex")
     var requirementSex : Boolean?,
 
+    @field:Pattern(regexp = "FIRST_COME|DIRECT_SELECTION")
     @Column(name = "recruit_type")
     var recruitType : RecruitType,
 
+    @field:Min(1)
     @Column(name = "recruit_num")
     var recruitNum : Long,
 
     @OneToMany
     var categories : List<CategoryMap>?
-
     ) {
 
     @Column(name = "state")
@@ -68,9 +79,12 @@ class MvpTest(
     @Column(name = "reject_reason")
     lateinit var rejectReason : String
 
-
-
-
+    init {
+        validateRecruitDate()
+        validateTestDate()
+        validateAgeRule()
+        validateCategoriesRule()
+    }
 
     fun update(request: UpdateMvpTestRequest, categoryMaps: List<CategoryMap>){
         mvpName = request.mvpName
@@ -88,5 +102,38 @@ class MvpTest(
         recruitType = RecruitType.fromString(request.recruitType)
         recruitNum = request.recruitNum
         categories = categoryMaps
+
+        validateRecruitDate()
+        validateTestDate()
+        validateAgeRule()
+        validateCategoriesRule()
     }
+
+    private fun validateRecruitDate(){
+        require(recruitEndDate.isAfter(recruitStartDate)&&recruitEndDate.isAfter(LocalDateTime.now())){
+            "Recruit end date is invalid"
+        }
+    }
+
+    private fun validateTestDate(){
+        require(testStartDate.isAfter(recruitStartDate)&&
+            testEndDate.isAfter(testStartDate)&&
+            testEndDate.isAfter(recruitEndDate)){
+            "Test date is invalid"
+        }
+    }
+
+    private fun validateAgeRule(){
+        require(requirementMaxAge == null || requirementMinAge == null || requirementMaxAge!! > requirementMinAge!!) {
+            "Maximum age must be greater than the minimum age."
+        }
+    }
+
+    private fun validateCategoriesRule() {
+        require(categories == null || categories!!.size <= 3) {
+            "The number of categories can be specified up to 3."
+        }
+    }
+
+
 }
