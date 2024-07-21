eb9be6a1a2da56ab6949aaf27ec2dfcd138fb1da
pcouqc <nagapen@nate.com>
2024-07-21 15:25:05 +0900

    :sparkles: feat : Add jpa for find duplicated report

--- src/main/kotlin/com/team1/mvp_test/domain/report/repository/ReportMediaRepository.kt
+++ src/main/kotlin/com/team1/mvp_test/domain/report/repository/ReportMediaRepository.kt
@@ -2,7 +2,9 @@ package com.team1.mvp_test.domain.report.repository
 
 import com.team1.mvp_test.domain.report.model.ReportMedia
 import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.stereotype.Repository
 
+@Repository
 interface ReportMediaRepository : JpaRepository<ReportMedia, Long> {
 
 }
