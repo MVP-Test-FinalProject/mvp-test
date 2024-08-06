c5bd41b932e39055b52aa4eb7fa08988a02252dd
AWKRID <minzoo202738@gmail.com>
2024-08-06 15:28:36 +0900

    :bug: fix: Add Enumerated annotation to Enterprise state

--- src/main/kotlin/com/team1/mvp_test/domain/enterprise/model/Enterprise.kt
+++ src/main/kotlin/com/team1/mvp_test/domain/enterprise/model/Enterprise.kt
@@ -24,6 +24,7 @@ class Enterprise(
     @Column(name = "phone_number")
     var phoneNumber: String,
 
+    @Enumerated(EnumType.STRING)
     @Column(name = "state")
     var state: EnterpriseState = EnterpriseState.PENDING,
 
