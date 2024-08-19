799d67a3a476c7e3d003ea8605894698dc9e8339
AWKRID <minzoo202738@gmail.com>
2024-08-19 11:25:01 +0900

    :bug: fix: Update MvpTestResponse to return enterpriseName

--- src/main/kotlin/com/team1/mvp_test/domain/mvptest/dto/MvpTestResponse.kt
+++ src/main/kotlin/com/team1/mvp_test/domain/mvptest/dto/MvpTestResponse.kt
@@ -1,12 +1,14 @@
 package com.team1.mvp_test.domain.mvptest.dto
 
+import com.team1.mvp_test.domain.enterprise.model.Enterprise
 import com.team1.mvp_test.domain.mvptest.model.MvpTest
 import com.team1.mvp_test.domain.mvptest.model.RecruitType
 import java.time.LocalDateTime
 
 data class MvpTestResponse(
-    val id: Long?,
-    val enterpriseId: Long?,
+    val id: Long,
+    val enterpriseId: Long,
+    val enterpriseName: String,
     val mvpName: String,
     val recruitStartDate: LocalDateTime,
     val recruitEndDate: LocalDateTime,
@@ -24,10 +26,11 @@ data class MvpTestResponse(
     val categories: List<String>
 ) {
     companion object {
-        fun from(mvpTest: MvpTest, categories: List<String>): MvpTestResponse {
+        fun from(mvpTest: MvpTest, enterprise: Enterprise, categories: List<String>): MvpTestResponse {
             return MvpTestResponse(
-                id = mvpTest.id,
+                id = mvpTest.id!!,
                 enterpriseId = mvpTest.enterpriseId,
+                enterpriseName = enterprise.name,
                 mvpName = mvpTest.mvpName,
                 recruitStartDate = mvpTest.recruitStartDate,
                 recruitEndDate = mvpTest.recruitEndDate,
